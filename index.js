c8c4f385f4678ec1bf51a6f05b075ab3a9cd3d38
DavidLucas1122 <davidlucased@gmail.com>
2026-01-27 22:45:30 -0300

    Pré Responsividade

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+function trocarTema() {
+    const variaveisCss = document.documentElement
+
+    const temaAtual = getComputedStyle(variaveisCss).getPropertyValue('--color-back')
+
+    if (temaAtual == '#e6e6e6') {
+
+        document.documentElement.style.setProperty(
+            "--color-primaria",
+            "#e6316d",
+        )
+        document.documentElement.style.setProperty(
+            "--color-back",
+            "#242424"
+        )
+        document.documentElement.style.setProperty(
+            "--color-text-default",
+            "#fff"
+        )
+        document.documentElement.style.setProperty(
+            "--color-secundaria",
+            "#5e4343df"
+        )
+    } else {
+        document.documentElement.style.setProperty(
+            "--color-primaria",
+            "#1F6FEB",
+        )
+        document.documentElement.style.setProperty(
+            "--color-back",
+            "#e6e6e6"
+        )
+        document.documentElement.style.setProperty(
+            "--color-text-default",
+            "#000"
+        )
+        document.documentElement.style.setProperty(
+            "--color-secundaria",
+            "#a5c4f0"
+        )
+    }
+}
